9e0bd2901da3e6dcf6196b6a192a5e99cb4aab20
Dillon Newell <dillon.newell@slalom.com>
2022-03-22 20:54:29 -0400

    adopt newer dotnet conventions

--- src/Life/Life.cs
+++ src/Life/Life.cs
@@ -1,18 +1,9 @@
-﻿using System;
+﻿using Life;
 
-namespace Life
-{
-    class Life
-    {
-        static void Main(string[] args)
-        {
-            Board board = Board.Random();
-            Console.WriteLine("State 0");
-            board.Print();
-            
-            board.Evolve();
-            Console.WriteLine("State 1");
-            board.Print();
-        }
-    }
-}
+var board = Board.Random();
+Console.WriteLine("State 0");
+board.Print();
+
+board.Evolve();
+Console.WriteLine("State 1");
+board.Print();
