ad118fdea768a1e46a2d9ffdb2672ced2315a92b
Dillon Newell <dillon@newell-labs.ca>
2022-04-11 21:24:45 +0000

    Revert "number tests"
    This reverts commit 6459da8f7ae4c697e9353815df0a159ce674abde.

--- src/Life.Tests/BoardTest.cs
+++ src/Life.Tests/BoardTest.cs
@@ -7,7 +7,7 @@ namespace Life.Tests;
 public class BoardTest
 {
     [Fact]
-    public void Step_1_Should_Initialize_Board()
+    public void Should_Initialize_Board()
     {
         var board = new Board(TestBoards.Board3x3);
         Assert.True(board.Rows == 3);
@@ -16,7 +16,7 @@ public class BoardTest
     }
     
     [Fact]
-    public void Step_2_Should_Initialize_Random_Board()
+    public void Should_Initialize_Random_Board()
     {
         var board = Board.Random();
         Assert.True(board.Rows == 10);
@@ -28,33 +28,33 @@ public class BoardTest
     }
 
     [Fact]
-    public void Step_3_Random_Board_Initialized_With_0_Size_Should_Throw()
+    public void Random_Board_Initialized_With_0_Size_Should_Throw()
     {
         Assert.Throws<ArgumentException>(() => Board.Random(0, 0));
     }
 
     [Fact]
-    public void Step_4_Board_Initialized_With_Null_Should_Throw()
+    public void Board_Initialized_With_Null_Should_Throw()
     {
         Assert.Throws<ArgumentNullException>(() => new Board(null!));
     }
 
     [Fact]
-    public void Step_5_Invalid_Initial_Board_Should_Throw()
+    public void Invalid_Initial_Board_Should_Throw()
     {
         int[,] arr = { { 0, 0, 0, 0, 1 }, {2, 0, 1, -1, 3} };
         Assert.Throws<ArgumentException>(() => new Board(arr));
     }
 
     [Fact]
-    public void Step_6_Initial_Board_Invalid_Size_Should_Throw()
+    public void Initial_Board_Invalid_Size_Should_Throw()
     {
         var state = new int[0,0];
         Assert.Throws<ArgumentException>(() => new Board(state));
     }
 
     [Fact]
-    public void Step_7_Board_Should_Evolve_To_Correct_State()
+    public void Board_Should_Evolve_To_Correct_State()
     {
         var board = new Board(TestBoards.Board3x3);
         board.Evolve();
