c0a6962b06aafa9a3907c13d96551ec624654c28
Pedro Osternack Correa <pedro.oc@slalom.com>
2022-04-27 17:37:51 -0400

    organizing the tests a bit

--- src/Life.Tests/BoardTest.cs
+++ src/Life.Tests/BoardTest.cs
@@ -4,59 +4,74 @@ using Life.Tests.TestModels;
 
 namespace Life.Tests;
 
-public class BoardTest
+[Collection("Board Initialization")]
+public class Constructor
 {
+    [Fact(DisplayName = "The board should receive an initial state.")]
+    public void NullInitialState_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Board(null!));
+    }
+
+    [Fact(DisplayName = "The board initial state should be bigger than a 0x0 matrix.")]
+    public void ZeroSizedInitialState_ThrowsArgumentException()
+    {
+        var state = new int[0,0];
+        Assert.Throws<ArgumentException>(() => new Board(state));
+    }
+
+    [Fact(DisplayName = "The board initial state cells should have one of two states, 1 (alive) or 0 (dead)")]
+    public void InitialBoardStateWithInvalidStates_ThrowsArgumentException()
+    {
+        int[,] arr = { { 0, 3 }, { 2, -1 } };
+        Assert.Throws<ArgumentException>(() => new Board(arr));
+    }
+
     [Theory]
     [MemberData(nameof(TestBoards.BoardScenarios), MemberType = typeof(TestBoards))]
-    public void Should_Initialize_Board(int size, int[,] initialState, int[,] evolvedState)
+    public void ValidInitialState_ShouldInitializeBoardState(int size, int[,] initialState, int[,] evolvedState)
     {
         var board = new Board(initialState);
         Assert.True(board.Rows == size);
         Assert.True(board.Columns == size);
         Assert.Equal(initialState, board.State);
     }
+}
 
-    [Fact]
-    public void Board_Initialized_With_Null_Should_Throw()
-    {
-        Assert.Throws<ArgumentNullException>(() => new Board(null!));
-    }
-
-    [Fact]
-    public void Random_Board_Initialized_With_0_Size_Should_Throw()
+[Collection("Board Generation")]
+public class Random
+{
+    [Fact(DisplayName = "Can only initialize a board bigger than 0x0")]
+    public void ZeroAndZero_ThrowsArgumentExcpetion()
     {
         Assert.Throws<ArgumentException>(() => Board.Random(0, 0));
     }
 
-    [Fact]
-    public void Invalid_Initial_Board_Should_Throw()
-    {
-        int[,] arr = { { 0, 0, 0, 0, 1 }, {2, 0, 1, -1, 3} };
-        Assert.Throws<ArgumentException>(() => new Board(arr));
-    }
-
-    [Fact]
-    public void Initial_Board_Invalid_Size_Should_Throw()
-    {
-        var state = new int[0,0];
-        Assert.Throws<ArgumentException>(() => new Board(state));
-    }
-
-    [Fact]
-    public void Should_Initialize_Random_Board()
+    [Fact(DisplayName = "When no size is defined, initializes a 10x10 board")]
+    public void NoArguments_ReturnsA10x10Board()
     {
         var board = Board.Random();
+
         Assert.True(board.Rows == 10);
         Assert.True(board.Columns == 10);
-        
+    }
+
+    [Fact(DisplayName = "When a size is passed in, initializes a board of that size")]
+    public void FiveAndFive_ReturnsA5x5Board()
+    {
         var customSizedBoard = Board.Random(5, 5);
+
         Assert.True(customSizedBoard.Rows == 5);
         Assert.True(customSizedBoard.Columns == 5);
     }
+}
 
+[Collection("Board Evolution")]
+public class Evolve
+{
     [Theory]
     [MemberData(nameof(TestBoards.BoardScenarios), MemberType = typeof(TestBoards))]
-    public void Board_Should_Evolve_To_Correct_State(int size, int[,] initialState, int[,] evolvedState)
+    public void CalculatesTheNextStateForTheBoard(int size, int[,] initialState, int[,] evolvedState)
     {
         var board = new Board(initialState);
         board.Evolve();
